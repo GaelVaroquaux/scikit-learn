e5e3b0feecc622d47de5c3839c3ecdd132d8aeba
Fabian Pedregosa <fabian@fseoane.net>
2013-05-27 14:01:36 +0200

    BUG: merge changes in tron.h from pytron

--- sklearn/svm/src/liblinear/tron.h
+++ sklearn/svm/src/liblinear/tron.h
@@ -15,17 +15,22 @@ public:
 class TRON
 {
 public:
-	TRON(const function *fun_obj, double eps = 0.1, int max_iter = 1000);
+	TRON(const function *fun_obj, double tol=0.1, double gtol = 0.1,
+	int max_iter = 1000);
 	~TRON();
 
-	void tron(double *w);
+	void tron(double *w, double *g);
 	void set_print_string(void (*i_print) (const char *buf));
+	int n_iter;
+	double gnorm;
+	double fun;
 
 private:
 	int trcg(double delta, double *g, double *s, double *r);
 	double norm_inf(int n, double *x);
 
-	double eps;
+	double gtol;
+	double tol;
 	int max_iter;
 	function *fun_obj;
 	void info(const char *fmt,...);
